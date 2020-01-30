57dac6b4c0f25bab49a8b33a5543b0b002822671
Mike Kolesnik <mike.kolesnik@gmail.com>
2020-01-30 22:28:02 +0200

    Implemented the download logic

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import time
+import subprocess
+import sys
 
 from threading import Thread
 
@@ -19,8 +21,34 @@ class Server(Thread):
 
     def _process_next_task(self):
         if not self.tasks:
-            time.sleep(1000)
+            time.sleep(10)
             return
+        
+        for task_id in self.tasks:
+            try:
+                self._run_download(task_id)
+            except:
+                print("Error while running:", sys.exc_info())
+
+        time.sleep(1000)
+    
+    def _run_download(self, task_id):
+        task = self.tasks[task_id]
+        youtubedl_cmd = ['./youtube-dl',
+            '--config-location', '.youtube-dl.config',
+            '--download-archive', 'youtube-dl.%s.archive' % task_id]
+        if 'args' in task:
+            youtubedl_cmd.extend(task['args'])
+
+        youtubedl_cmd.append(task['url'])
+        out = subprocess.Popen(youtubedl_cmd, 
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+        
+        stdout, stderr = out.communicate()
+        task['returncode'] = out.returncode
+        task['stderr'] = str(stderr)
+        task['stdout'] = str(stdout)
 
     def is_following(self, task_id):
         return task_id in self.tasks
