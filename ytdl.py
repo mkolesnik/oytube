5de096f497a8a328bcd5bbc5ab365b2ab767b335
Mike Kolesnik <mike.kolesnik@gmail.com>
2020-02-08 21:46:47 +0200

    Added backoff for checks

--- ytdl.py
+++ ytdl.py
@@ -1,6 +1,9 @@
-import datetime
 import os
 import youtube_dl
+
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
 
 GLOBAL_OPTS = {
     "ignoreerrors": True,
@@ -11,6 +14,7 @@ GLOBAL_OPTS = {
     "writeinfojson": True,
     "outtmpl": '%(title)s.%(ext)s',
 }
+EPOCH = datetime.fromtimestamp(0).timestamp()
 
 class TaskLogger(object):
     def __init__(self, task):
@@ -54,6 +58,14 @@ def get_directory(task):
     )
 
 def download(task_id, task):
+    logger = TaskLogger(task)
+    now = datetime.now().timestamp()
+    last_checked = task.get('last_checked', EPOCH)
+    if last_checked > now - timedelta(hours=3).total_seconds():
+        logger.debug('[OYTube] Already checked recently, skipping')
+        return
+    task['last_checked'] = now
+
     ydl_opts = dict(GLOBAL_OPTS)
     ydl_opts.update({
         'download_archive': os.path.join(
@@ -61,7 +73,7 @@ def download(task_id, task):
             'youtube-dl.%s.archive' % task_id
         ),
         'daterange': youtube_dl.utils.DateRange(start=task.get('last_run')),
-        'logger': TaskLogger(task),
+        'logger': logger,
     })
     ydl_opts.update(task.get('opts', {}))
 
@@ -71,7 +83,7 @@ def download(task_id, task):
         os.makedirs(dl_dir, exist_ok=True)
         os.chdir(dl_dir)
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            task['last_run'] = datetime.date.today().strftime('%Y%m%d')
+            task['last_run'] = date.today().strftime('%Y%m%d')
             task['return_code'] = ydl.download([task['url']])
     finally:
         os.chdir(orig_dir)
